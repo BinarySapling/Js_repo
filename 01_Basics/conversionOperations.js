c40410f19ec13bf1fdfa00458291c42231448a9c
BinarySapling <danishanwarofficial@gmail.com>
2025-02-16 19:04:22 +0530

    learnt conversion basics

---
+++ 01_Basics/conversionOperations.js
@@ -0,0 +1,17 @@
+// let score="true";
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>Nan
+//true=>none
+// let isLoggedIn=NaN;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+
+// console.log(booleanIsLoggedIn);
+let sumNumber=null;
+let stringNumber=String(sumNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
