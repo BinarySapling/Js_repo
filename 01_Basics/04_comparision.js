a3a7456ef65f10d9861a4bd2377276801eca84fe
BinarySapling <danishanwarofficial@gmail.com>
2025-02-16 20:16:12 +0530

    learnt strict comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log("2"==="3");
+// console.log(null==0)
+// console.log(null>0);
+// console.log(null>=0);//unpredicted result due to error in conversion
+// //the == and the >= < >= <= work differently . Comparisions convert null to a number treating it as 0;
+
+// console.log(undefined >=0)
+//strict check
+// console.log('2' == 2);
+// console.log('2' === 2);
