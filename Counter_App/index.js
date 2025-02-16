e2d981d844eac8f65e9dd26905ca34e65cff040c
BinarySapling <danishanwarofficial@gmail.com>
2025-02-17 02:31:59 +0530

    .

--- Counter_App/index.js
+++ Counter_App/index.js
@@ -1,4 +1,9 @@
 let count =0;
+function save(){
+    let countstr=count + "-"
+    document.getElementById("save-el").textContent+=countstr;
+    console.log(count)
+}
 function increment(){
     count++;
     document.getElementById("count-el").innerHTML=count;;
