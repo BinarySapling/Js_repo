b443e3b3033a41e9c04ddc27150d8e204bc1d9cc
BinarySapling <danishanwarofficial@gmail.com>
2025-02-16 19:44:30 +0530

    learnt string operations

--- 01_Basics/conversionOperations.js
+++ 01_Basics/conversionOperations.js
@@ -11,7 +11,15 @@
 
 
 // console.log(booleanIsLoggedIn);
-let sumNumber=null;
-let stringNumber=String(sumNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let sumNumber=null;
+// let stringNumber=String(sumNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+// let value=3;
+// let negValue= -value;
+// console.log(negValue);
+// let str1=1;
+// let str2="1";
+// console.log(1+2+"2"+"5");
+// console.log(+true)
+//console.log(+ "")
