0e9e8f91e41e078d9205d9995dcf31defe29ba4b
Md Danish Anwar <danishanwarofficial@gmail.com>
2025-02-16 18:36:44 +0530

    learnt datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer versions
+//alert(3+3)we r using nodeJs and not browser
+
+let Name="Danish";
+let age=14;
+let isLoggedIn=true;
+/*primitive data types
+
+//number => 2 pow(53) range
+//bigint
+//string=>""
+//boolean :true or false
+//null:standalone value:typeOf=object
+//undefined=>when value not defined; 
+//symbol =>used for uniqueness
+
+*/
+//object
+console.log(typeof undefined);
